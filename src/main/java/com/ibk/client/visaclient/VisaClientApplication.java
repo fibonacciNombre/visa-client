288877ae9533b97920c337eb654bc6fe41a0555c
Inga Ninahuanca <dingan@intercorp.com.pe>
2019-03-08 14:26:19 -0500

    first deploy

---
+++ src/main/java/com/ibk/client/visaclient/VisaClientApplication.java
@@ -0,0 +1,14 @@
+package com.ibk.client.visaclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VisaClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VisaClientApplication.class, args);
+	}
+
+}
+
